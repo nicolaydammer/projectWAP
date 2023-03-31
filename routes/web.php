529088eeafc95fbbcddce406a02e87ff95e97f32
Pjotr <kooijmans97@gmail.com>
2023-03-31 15:36:29 +0200

    added Breeze authentication and admin panel with CRU for users.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::resource('users', \App\Http\Controllers\UserController::class)
+    ->only(['index', 'edit', 'update', 'create', 'store','destroy'])
+    ->middleware(['auth', 'verified', 'role:admin']);
+
+require __DIR__.'/auth.php';
