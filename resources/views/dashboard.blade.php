af75e04b71a4add00c7846e994c03c7003596bf5
AlwinStecher <alwinstecher@icloud.com>
2023-04-03 15:15:26 +0200

    login tweede versie

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,35 @@
 
 @include("partial.header")
-<h1>Dashboard</h1>
-<p>{{ $name }}</p>
- @include("partial.footer")
+<h1 id="dashboard-title">Dashboard</h1>
+<p id="dashboard-welkom-text">Welkom, {{ $name }}</p>
+
+<table class="dashbord-table">
+    <tr class="vakjes">
+        <th>Weerstation</th>
+        <th>Status</th>
+        <th>Land</th>
+    </tr>
+    <tr class="vakjes">
+        <td>248852</td>
+        <td>Online</td>
+        <td>Groningen</td>
+    </tr>
+    <tr class="vakjes">
+        <td>265440</td>
+        <td>Error!</td>
+        <td>Maastricht</td>
+    </tr>
+    <tr class="vakjes">
+        <td>385647</td>
+        <td>Online</td>
+        <td>New York</td>
+    </tr>
+    <tr class="vakjes">
+        <td>429850</td>
+        <td>Online</td>
+        <td>Caïro</td>
+    </tr>
+</table>
+
+
+@include("partial.footer")
--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,5 +1,14 @@
 <footer>
-    <p>Dit is de footer</p>
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="/login" class="nav-link px-2 text-muted">Login</a></li>
+            <li class="nav-item"><a href="/dashboard" class="nav-link px-2 text-muted">Dashboard</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Wetenschapper</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Administratie</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Over</a></li>
+        </ul>
+        <p class="text-center text-muted">© 2023 IWA Company</p>
+    </footer>
 </footer>
 </body>
 </html>
