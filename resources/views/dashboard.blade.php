3692f7fcec998f26568890e0f5399ab3473a1d11
nicolaydammer <nicolaydammer@gmail.com>
2023-04-10 15:53:34 +0200

    CHANGE: Made the code more readable

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,22 +20,36 @@
 
     <nav aria-label="Page navigation example">
         <ul class="pagination">
-            <li class="page-item @if($stations->currentPage() === 1) disabled @endif"><a class="page-link"
-                   href="{{ route('dashboard', ['page' => 1]) }}">Eerste</a>
+            <li class="page-item @if($stations->currentPage() === 1) disabled @endif">
+                <a class="page-link" href="{{ route('dashboard', ['page' => 1]) }}">
+                    Eerste
+                </a>
             </li>
-            <li class="page-item @if($stations->currentPage() === 1) disabled @endif"><a class="page-link"
-                                                                                         href="{{ route('dashboard', ['page' => $stations->currentPage() - 1]) }}">Vorige</a>
+            <li class="page-item @if($stations->currentPage() === 1) disabled @endif">
+                <a class="page-link"
+                   href="{{ route('dashboard', ['page' => $stations->currentPage() - 1]) }}">
+                    Vorige
+                </a>
             </li>
-            @for($i = ($stations->currentPage() >= 2 ? $stations->currentPage() - 1 : 1);$i < $stations->currentPage() + ($stations->currentPage() > 1 ? 2 : 3); $i++)
+            @for(
+    $i = ($stations->currentPage() >= 2 ? $stations->currentPage() - 1 : 1);
+    $i < $stations->currentPage() + ($stations->currentPage() > 1 ? 2 : 3);
+    $i++)
                 <li class="page-item @if($stations->currentPage() === $i) active @endif @if($i > $stations->lastPage()) disabled @endif">
-                    <a class="page-link" href="{{ route('dashboard', ['page' => $i]) }}">{{ $i }}</a></li>
+                    <a class="page-link" href="{{ route('dashboard', ['page' => $i]) }}">
+                        {{ $i }}
+                    </a>
+                </li>
             @endfor
-            <li class="page-item @if($stations->currentPage() === $stations->lastPage()) disabled @endif"><a
-                        class="page-link"
-                        href="{{ route('dashboard', ['page' => $stations->currentPage() + 1]) }}">Next</a></li>
-            <li class="page-item @if($stations->currentPage() === $stations->lastPage()) disabled @endif"><a
-                        class="page-link"
-                        href="{{ route('dashboard', ['page' => $stations->lastPage()]) }}">Laatste</a>
+            <li class="page-item @if($stations->currentPage() === $stations->lastPage()) disabled @endif">
+                <a class="page-link" href="{{ route('dashboard', ['page' => $stations->currentPage() + 1]) }}">
+                    Next
+                </a>
+            </li>
+            <li class="page-item @if($stations->currentPage() === $stations->lastPage()) disabled @endif">
+                <a class="page-link" href="{{ route('dashboard', ['page' => $stations->lastPage()]) }}">
+                    Laatste
+                </a>
             </li>
         </ul>
     </nav>
