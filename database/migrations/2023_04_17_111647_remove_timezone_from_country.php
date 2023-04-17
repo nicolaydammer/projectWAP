c2e07412d4c7db1a540091c0595c17702e0c1749
nicolaydammer <nicolaydammer@gmail.com>
2023-04-17 16:07:17 +0200

    ADD: bugfix

--- database/migrations/2023_04_17_111647_remove_timezone_from_country.php
+++ database/migrations/2023_04_17_111647_remove_timezone_from_country.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('countries', function (Blueprint $table) {
-            $table->removeColumn('timezone');
+            $table->dropColumn('timezone');
         });
     }
 };
