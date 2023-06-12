1f81fb41777ccc7e3e6a2c786e4540fea5c8aa40
timdewilde <tim-de-wilde@live.nl>
2023-06-12 21:00:10 +0200

    made an endpoint for external authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Auth\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/postWheatherData', 'App\Http\Controllers\WheatherDataController@index');
+
+Route::post('/api/login', [AuthenticationController::class, 'login']);
 
 Route::group([
     'prefix' => '/iwa',
