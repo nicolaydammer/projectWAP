7717d38cb619ebb399a32556d07b3b4b40626b63
timdewilde <tim-de-wilde@live.nl>
2023-06-15 17:43:16 +0200

    fixed middleware in api routes and changed a route endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/postWheatherData', 'App\Http\Controllers\WheatherDataController@index');
 
-Route::post('/api/login', [AuthenticationController::class, 'login']);
+Route::post('/iwa/login', [AuthenticationController::class, 'login']);
 
 Route::group([
     'prefix' => '/iwa',
-    'middleware' => [
-        'verifyToken',
-        ],
+    'middleware' => ['auth:sanctum'],
     ], function () {
 
     Route::group(['prefix' => '/abonnement'], function () {
